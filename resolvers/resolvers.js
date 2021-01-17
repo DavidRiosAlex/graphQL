f718a94aaf43ac89a463963ce26c98c0a7a406ca
David Alexander Rios <drios@globalthinktec.com>
2021-01-17 03:42:14 -0300

    new auths

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -15,8 +15,16 @@ const Resolvers = {
       const user = await UserModel(input)
       await user.save()
       return user
-    }
-  }
-};
+    },
+    token(data){
+      console.log(data);
+      if (!(data && data.username && data.password)) throw {status: 404, error:'BAD REQUEST'};
+      
+      const user = UserModel.findOne({username: data.username });
+
+      if (!user.authenticate(data.password)) throw {status:401, error:'NOT AUTHORIZED'};
+
+      return {accessToken: user.getToken().token, refreshToken: user.getToken().refreshToken, user}
+}};
 
 export default Resolvers;
